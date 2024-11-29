ff0b6fb26e80640dbe55409b840cff6304369898
Hung Le Ton <HungLT2@fpts.com.vn>
2024-11-29 09:12:16 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const courses = [
+  {
+    id: 1,
+    name: "HTML,CSS",
+  },
+  {
+    id: 2,
+    name: "Javascript",
+  },
+  {
+    id: 3,
+    name: "ReactJS",
+  },
+];
 
 function App() {
+  const gifts = ["CPU i9", "RAM 32GB RGB", "RGB Keyboard"];
+  const [gift, setGift] = useState();
+  const [email, setEmail] = useState();
+  const handleSubmit = () => {
+    console.log({
+      name,
+      email,
+    });
+  };
+  const handleUpdate = () => {
+    const index = Math.floor(Math.random() * gifts.length);
+    setGift(gifts[index]);
+  };
+  const [name, setName] = useState();
+  const [checked, setChecked] = useState();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      {courses.map((courses) => (
+        <div key={courses.id}>
+          <input
+            type="checkbox"
+            checked={checked === courses.id}
+            onchange={() => setChecked(courses.id)}
+          />
+          {courses.name}
+        </div>
+      ))}
+      <h1>{gift || "Chưa có phần thưởng"}</h1>
+      <button onClick={handleUpdate}>Lấy thưởng</button>
+      <br></br>
+      <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+      <button onClick={handleSubmit}> Change</button>
     </div>
   );
 }
-
 export default App;
